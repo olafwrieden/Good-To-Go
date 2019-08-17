ee38bc19bf84281cd202aa0e5c6bea6276ff35d2
Ashley Harris <ashleyharrisnz@gmail.com>
2019-08-17 13:42:31 +1200

    find lat long

--- client/scripts.js
+++ client/scripts.js
@@ -1,1 +1,12 @@
-alert("helloworld");
+new google.maps.event.addListener(map, 'click', function (event) {
+
+    // marker = new google.maps.Marker({
+    //     position: event.latLng,
+    //     map: map
+    // });
+
+    let lat = event.latLng.lat();
+    let lng = (event.latLng.lng());
+    console.log(lat, lng);
+
+});
