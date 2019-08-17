2c8f31a04f35d5982e3ae0b7f7c452c3886a2f8a
Olaf Wrieden <42084239+olafwrieden@users.noreply.github.com>
2019-08-18 11:53:41 +1200

    Rearrange for heroku

--- server/app.js
+++ server/app.js
@@ -12,12 +12,13 @@ const port = 3000;
 
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static('public'))
 // Default Route, No Data!
 app.get('/', (req, res) => {
-  res
-    .status(200)
-    .send('No data to display here. Give /info or /dev/info a try :)');
+  res.sendFile(__dirname + '/public/index.html');
+  // res
+  //   .status(200)
+  //   .send('No data to display here. Give /info or /dev/info a try :)');
 });
 
 // Returns the weather info for the given coordinates. TODO: Switch Over To Live Data
---
+++ server/public/scripts.js
@@ -0,0 +1,101 @@
+var markers = [];
+var map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -36.848461, lng: 174.763336 },
+        zoom: 8
+    });
+
+    google.maps.event.addListener(map, "click", function(event) {
+        clearMarkers();
+        addMarker(event.latLng);
+
+        // Bring out drawer, remove obfuscator
+        $(".mdl-layout__drawer").addClass("is-visible");
+        $(".mdl-layout__obfuscator").removeClass("is-visible");
+
+        getInfo(event);
+    });
+}
+
+// Adds a marker to the map and push to the array.
+var addMarker = location => {
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map
+    });
+    markers.push(marker);
+};
+
+// Sets the map on all markers in the array.
+var setMapOnAll = map => {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+    }
+};
+
+// Removes the markers from the map, but keeps them in the array.
+var clearMarkers = () => {
+    setMapOnAll(null);
+};
+
+getInfo = event => {
+    let lat = event.latLng.lat();
+    let lon = event.latLng.lng();
+    console.log(lat, lon);
+
+    fetch(`http://localhost:3000/dev/info?lat=${lat}&lon=${lon}`)
+        .then(response => {
+            if (response.status !== 200) {
+                console.log(
+                    "There was a problem, status code: " + response.status
+                );
+                return;
+            }
+            response.json().then(data => {
+                console.log(data);
+                updateInfo(data);
+            });
+        })
+        .catch(err => console.log);
+};
+
+// add stuff here that needs to be done after document load
+$(document).ready(function() {
+    $(".drawer-button").click(() => {
+        $(".mdl-layout__drawer").removeClass("is-visible");
+        clearMarkers();
+    });
+});
+
+var updateInfo = data => {
+    $("#temp_apparent").html(data.weather.temp_apparent);
+    $("#temp_current").html(data.weather.temp_current);
+    $("#temp_high").html(data.weather.temp_high);
+    $("#temp_low").html(data.weather.temp_low);
+    $("#rainfall").html(data.weather.rainfall);
+    $("#text_description").html(data.weather.text_description);
+    $("#wind_dir").html(data.weather.wind_dir);
+    $("#wind_speed").html(data.weather.wind_speed);
+    $("#swell_height").html(data.marine.swell_height);
+    $("#water_temp").html(data.marine.water_temp);
+    $("#visibility").html(data.weather.visibility);
+    renderNearestCoastGuard(data.coastguard_stations);
+};
+var renderNearestCoastGuard = coastguard_stations => {
+    // find coast guard minimum distance away
+    let nearestCoastguard = coastguard_stations.reduce((prev, current) => {
+        return prev.distance < current.distance ? prev : current;
+    });
+
+    // render info
+    $("#coastguard-location").html(nearestCoastguard.station);
+    let distanceInKm = metresToKm(nearestCoastguard.distance);
+    $("#coastguard-distance").html(`${distanceInKm} km away`);
+};
+
+var metresToKm = distanceInKm => {
+    let distanceInM = distanceInKm / 1000;
+    return distanceInM.toFixed(1);
+};
