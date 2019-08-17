97f0c7d75ac9225479f642a6b8e3742e5dc15a2c
Harrison Leach <HarrisonL7190@gmail.com>
2019-08-17 14:40:44 +1200

    Add JQUERY

--- client/scripts.js
+++ client/scripts.js
@@ -1,24 +1,28 @@
-google.maps.event.addListener(map, 'click', function (event) {
+$(document).ready(function() {
+    new google.maps.event.addListener(map, "click", function(event) {
+        // marker = new google.maps.Marker({
+        //     position: event.latLng,
+        //     map: map
+        // });
 
-    // marker = new google.maps.Marker({
-    //     position: event.latLng,
-    //     map: map
-    // });
+        let lat = event.latLng.lat();
+        let lon = event.latLng.lng();
+        console.log(lat, lon);
 
-    let lat = event.latLng.lat();
-    let lon = (event.latLng.lng());
-    console.log(lat, lon);
+        fetch(`http://localhost:3000/info?lat=${lat}&lon=${lon}`)
+            .then(response => {
+                if (response.status !== 200) {
+                    console.log(
+                        "There was a problem, status code: " + response.status
+                    );
+                    return;
+                }
+                response.json().then(data => console.log);
+            })
+            .catch(err => console.log);
+    });
 
-    fetch(`http://localhost:3000/info?lat=${lat}&lon=${lon}`).then(response => {
-        if (response.status !== 200) {
-            console.log("There was a problem, status code: " + response.status);
-            return;
-        }
-        response.json().then(data => console.log);
-    }).catch(err => console.log);
-
+    function writeInfo() {
+        document.getElementById("p1").innerHTML = "<h2>Weather</h2>";
+    }
 });
-
-function writeInfo() {
-    document.getElementById("p1").innerHTML = "<h2>Weather</h2>";
-}
