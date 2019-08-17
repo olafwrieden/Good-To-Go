809d9154c568fbe0c115f62fae2861e4ce4edcdc
Jordan Sim-Smith <jordansimsmith@gmail.com>
2019-08-17 13:36:25 +1200

    Connected open weather api to retrieve weather data

--- server/app.js
+++ server/app.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const axios = require('axios');
+const d2d = require('degrees-to-direction');
+
+dotenv.config();
+
 const app = express();
 const port = 3000;
 
@@ -20,6 +26,41 @@ app.get('/info', (req, res) => {
   };
 
   res.status(200).send(response);
+});
+
+app.get('/dev/info', async (req, res) => {
+  const lat = req.query.lat;
+  const lon = req.query.lon;
+
+  if (Number(lat) < -86 || Number(lat) > 86) {
+    return res.status(400).send('invalid latitude');
+  }
+
+  if (Number(lon) < -180 || Number(lon) > 180) {
+    return res.status(400).send('invalid longitude');
+  }
+
+  const API_KEY = process.env.OPEN_WEATHER_KEY;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+  try {
+    const {data} = await axios.get(url);
+
+    const response = {
+      weather: {
+        temp_high: data.main.temp_max,
+        temp_low: data.main.temp_min,
+        temp_current: data.main.temp,
+        wind_speed: data.wind.speed,
+        wind_dir: d2d(data.wind.deg),
+        text_description: data.weather[0].description,
+      },
+    };
+
+    return res.status(200).send(response);
+  } catch (error) {
+    return res.status(502).send(error.message);
+  }
 });
 
 app.listen(port, () => console.log(`listening on port ${port}`));
